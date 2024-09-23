d0bff21fe27418f2d1858cd78dfcf171ffe2ffb6
Erhan Kesbic <erhan.kesbic@the-clapp.com>
2024-09-23 22:09:05 +0200

    nun

--- app/lists/page.tsx
+++ app/lists/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React, { useState, FormEvent } from 'react';
+import { Button } from "../../components/ui/button";
 
 const ListPage = () => {
   const [listName, setListName] = useState('');
